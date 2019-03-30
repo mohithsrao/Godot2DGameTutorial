d4dc24c6b9c1fd144bdbdc08756296aeca710c95
mohithsrao <mohithsrao@gmail.com>
2019-03-31 00:44:05 +0530

    Added Shortcuts and sound

--- First2DGame/Main.gd
+++ First2DGame/Main.gd
@@ -10,12 +10,17 @@ func _on_Player_hit():
 	game_over()
 
 func game_over():
+	$Music.stop()
+	$DeathSound.play()
+	
 	$ScoreTimer.stop()
 	$MobTimer.stop()
 	
 	$HUD.show_game_over()
 
-func new_game():	
+func new_game():
+	$Music.play()	
+	
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
